9b9982416c41c645b21f23f305211a87b5dd387c
Andrew Plotkin <zarf@ZarfLent.local>
2023-09-29 11:24:00 -0400

    Comment.

--- upload.py
+++ upload.py
@@ -11,6 +11,8 @@ errors. The Apache config contains the line "SetEnv LANG en_US.UTF-8", which
 takes care of this.
 """
 
+# Andrew Plotkin (29 Sep 2023):
+#   - Bumped upload-dir limit to one gigabyte.
 # Andrew Plotkin (27 May 2018):
 #   - Added an "accept the terms of service" checkbox.
 # Andrew Plotkin (23 November 2017):
